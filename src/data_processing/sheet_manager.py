d33e7cd7ef7268b51a18e59b924d4b562940f36e
joshuaiokua <jiokua.albano@gmail.com>
2024-08-24 23:53:05 -0700

    refactor: removed dfstore
    Removed dfstore class as this functionality is mostly covered by current polars and pandas implementation. However I can foresee I will want to implement some custom logic and have created a sheet_manager.py file with this expectation in mind.

---
+++ src/data_processing/sheet_manager.py
@@ -0,0 +1,5 @@
+"""
+data_processing/sheet_manager.py
+
+Module for managing separate Excel sheets that are part of the same data source, with the expectation that each sheet will be loaded into a separate DataFrame.
+"""
