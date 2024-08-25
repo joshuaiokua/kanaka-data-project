882e235cd819d9601cb48681fdddae417b379f9e
joshuaiokua <jiokua.albano@gmail.com>
2024-08-25 00:19:16 -0700

    init: first commit
    Simple name change

---
+++ src/data_processing/df_manager.py
@@ -0,0 +1,5 @@
+"""
+data_processing/df_manager.py
+
+Module for managing separate DataFrame objects that originate, or are somehow related to, the same data source (e.g. a single Excel file with multiple sheets).
+"""
