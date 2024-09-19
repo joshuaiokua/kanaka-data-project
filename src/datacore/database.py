7ce885820b75f1b59c07b2ab07c699382743930e
joshuaiokua <jiokua.albano@gmail.com>
2024-09-19 01:16:14 -0700

    feat: load df from local db

--- src/datacore/database.py
+++ src/datacore/database.py
@@ -5,16 +5,40 @@ Functions:
     load_local_database: Load a local SQLite database as a LangChain SQLDatabase object.
 """
 
-from pathlib import Path
-
 from langchain_community.utilities import SQLDatabase
-from sqlalchemy import create_engine
+from pandas import DataFrame, read_sql
+from sqlalchemy import Engine, create_engine
 from sqlalchemy.pool import StaticPool
 
 from src.constants.sources import DB_PATH
 
+from .utils import is_valid_table_name
+
 
 ### --- FUNCTIONS --- ###
+def create_local_engine(
+    database_file_path: str,
+    connect_args: dict | None = None,
+    poolclass: type = StaticPool,
+    **kwargs,
+) -> Engine:
+    """
+    Create a SQLAlchemy engine for a local SQLite database.
+
+    Args:
+        database_file_path (str): The path to the .db file.
+        connect_args (dict, optional): Additional connection arguments to pass to the `create_engine` function. If `None`, defaults to `{"check_same_thread": False}`.
+        poolclass (type, optional): The class to use for pooling connections. Defaults to `StaticPool`.
+        kwargs: Additional keyword arguments to pass to the `create_engine` function.
+    """
+    return create_engine(
+        f"sqlite:///{database_file_path}",
+        connect_args=connect_args or {"check_same_thread": False},
+        poolclass=poolclass,
+        **kwargs,
+    )
+
+
 def load_local_database(
     database_name: str,
     database_path: str = DB_PATH,
@@ -33,10 +57,43 @@ def load_local_database(
     Returns:
         SQLDatabase: The database object.
     """
-    engine = create_engine(
-        f"sqlite:///{database_path}/{database_name}.db",
-        connect_args=connect_args or {"check_same_thread": False},
-        poolclass=StaticPool,
-        **kwargs,
+    return SQLDatabase(
+        create_local_engine(
+            f"{database_path}/{database_name}.db",
+            connect_args=connect_args or {"check_same_thread": False},
+            poolclass=StaticPool,
+            **kwargs,
+        ),
     )
-    return SQLDatabase(engine)
+
+
+def load_df_from_local_db(
+    table_name: str,
+    database_name: str,
+    database_path: str = DB_PATH,
+) -> DataFrame:
+    """
+    Load a pandas DataFrame from a local SQLite database.
+
+    Args:
+        table_name (str): The name of the table in the database.
+        database_name (str): The name of the .db file.
+        database_path (str, optional): The path to the .db file. Defaults to `DB_PATH` (i.e. "data/databases/").
+
+    Returns:
+        DataFrame: The loaded DataFrame.
+    """
+    # Validate table name
+    if not is_valid_table_name(table_name):
+        msg = f"Invalid table name: {table_name}"
+        raise ValueError(msg)
+
+    engine = create_local_engine(
+        database_file_path=f"{database_path}/{database_name}.db",
+    )
+
+    with engine.connect() as connection:
+        return read_sql(
+            f"SELECT * FROM {table_name}",  # noqa: S608
+            connection,
+        )
---
+++ src/datacore/utils.py
@@ -0,0 +1,16 @@
+"""
+Utility functions for working with data and data-related tasks.
+
+Functions:
+    is_valid_table_name: Guards against SQL injection by checking validity of table name.
+"""
+
+import re
+
+
+### --- FUNCTIONS --- ###
+def is_valid_table_name(table_name: str) -> bool:
+    """
+    Check if the provided table name is a valid SQL identifier.
+    """
+    return bool(re.match(r"^[A-Za-z_][A-Za-z0-9_]*$", table_name))
