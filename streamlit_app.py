88881a5ee16a0e8b1935c8b85b4b9db4f3ea2c69
joshuaiokua <jiokua.albano@gmail.com>
2024-09-21 00:55:56 -0700

    feat: base chatbot framework

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,30 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-if "df" not in st.session_state:
-    st.session_state.df = pd.DataFrame(np.random.randn(20, 2), columns=["x", "y"])
+context = "***Mo'olelo***: the narrative stories of the Kanaka Maoli (i.e. Native Hawaiians)."
+st.title("Data Mo'olelo", help=context)
 
-st.header("Choose a datapoint color")
-color = st.color_picker("Color", st.get_option("theme.primaryColor"))
-st.divider()
-st.scatter_chart(st.session_state.df, x="x", y="y", color=color)
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Aloha, howzit?"},
+    ]
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = ":call_me_hand: :call_me_hand: :call_me_hand:"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
